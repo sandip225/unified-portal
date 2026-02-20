fb4f2d294ff65bf014a3926f12329f4c44d3f801
dipakbipinbhatt <dipakbipinbhatt@gmail.com>
2026-02-20 14:26:41 +0530

    model updated

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, JSON, Enum, Text
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, JSON, Enum, Text, Boolean
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from app.database import Base
